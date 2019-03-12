7a4ad4a044fd5871818e8935825b8ff5705ef5d4
joshu107 <joshu107@student.liu.se>
2019-03-12 14:56:35 +0100

    initialised link list
    save links as json for future updating

--- indeedAPI.py
+++ indeedAPI.py
@@ -6,9 +6,15 @@ Created on Mon Mar  4 17:59:15 2019
 """
 #libraries
 from urllib import request
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import re
+import os
+import json
+#import datetime
+#import time
 #from selenium import webdriver (if needed)
+#home directory-> repo folder
+homedir = "C:/Users/jbhud/Documents/GitHub/JobHunter/"
 
 #1. get search result pages
 #future function inputs
@@ -32,7 +38,12 @@ totalres = int(re.search("(?<=Sida 1 av )(\d+)(?= resultat)", homepage).group())
 totalpages = totalres//10+1  
     
 #2. extract job listing links
-links = {}
+if "links.json" in os.listdir(homedir+"Data"):
+    with open(homedir+"Data/links.json", "r") as f:
+        links = json.load(f)
+else:
+    links = {} #initial run
+    
 for page in range(totalpages):
     URL = homeURL+"&start="+str(page*10)
     with request.urlopen(URL) as f:
@@ -44,10 +55,14 @@ for page in range(totalpages):
         jobid = re.findall("id=\"(.*)\" data-jk=", job)[0]
         link = re.findall("\/rc\/clk\?jk=(.*)&vjs=3", job)[0]
         link = "https://se.indeed.com/rc/clk?jk="+link+"&vjs=3"
-        links[jobid] = link
-    
+        tempdict = {jobid:link}
+        tempdict.update(links)
+        links = tempdict #done to keep newest version of same job ad
+with open(homedir+"Data/links.json", "w") as f:
+    json.dump(links, f)
+
 #use of dictionary omits 17 similar results that Indeed omits really 96 results->79
 #PROBLEM: was planning on stopping search as soon as a job id is recognised, ie. already scraped job ad but the re-posting 
         #by companies means that i cant do this, i need to search through all each time
         #SOLUTION: use date posted? -> eg regex <span class="date">2 dagar sedan</span>
-#visit each job listing, check if new/unique (using id no?), extract job descriptions
+#visit each job listing, extract job descriptions
