7f3c98e31a37e012f1885d1c8a537e6e772cd2e0
joshu107 <joshu107@student.liu.se>
2019-03-13 19:19:51 +0100

    job descriptions extracted
    changed the way new links are added (from using dict key uniqueness to set inclusion (set of keys) test
    now need to change code to just extract desciptions for NEW LINKS ONLY (separate newlinks dict?)

--- indeedAPI.py
+++ indeedAPI.py
@@ -6,13 +6,14 @@ Created on Mon Mar  4 17:59:15 2019
 """
 #libraries
 from urllib import request
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import re
+#from selenium import webdriver (if needed)
 import os
 import json
 #import datetime
-#import time
-#from selenium import webdriver (if needed)
+import time
+
 #home directory-> repo folder
 homedir = "C:/Users/jbhud/Documents/GitHub/JobHunter/"
 
@@ -43,7 +44,8 @@ if "links.json" in os.listdir(homedir+"Data"):
         links = json.load(f)
 else:
     links = {} #initial run
-    
+
+newlinks = []    
 for page in range(totalpages):
     URL = homeURL+"&start="+str(page*10)
     with request.urlopen(URL) as f:
@@ -53,16 +55,41 @@ for page in range(totalpages):
     for job in listings:
         #jobid = re.search("(?<=id=\")(.*)(?=\" data-jk=)", job).group() #(LOOKAROUND PRACTISE)
         jobid = re.findall("id=\"(.*)\" data-jk=", job)[0]
-        link = re.findall("\/rc\/clk\?jk=(.*)&vjs=3", job)[0]
-        link = "https://se.indeed.com/rc/clk?jk="+link+"&vjs=3"
-        tempdict = {jobid:link}
-        tempdict.update(links)
-        links = tempdict #done to keep newest version of same job ad
+        if jobid not in set(links.keys()):
+            link = re.findall("\/rc\/clk\?jk=(.*)&vjs=3", job)[0]
+            link = "https://se.indeed.com/rc/clk?jk="+link+"&vjs=3"
+            newlinks = newlinks+[link]
+            tempdict = {jobid:link}
+            links.update(tempdict) #potential issue:keeps oldest version of ad
+        #new method assumes we did all our analysis on previously found links!
+        #so be careful in development phase (could just erase links.json until fully ready)
 with open(homedir+"Data/links.json", "w") as f:
     json.dump(links, f)
 
-#use of dictionary omits 17 similar results that Indeed omits really 96 results->79
-#PROBLEM: was planning on stopping search as soon as a job id is recognised, ie. already scraped job ad but the re-posting 
+#use of dictionary (key uniqueness) omits 17 similar results that Indeed omits really 96 results->79
+#FUTURE PROBLEM: was planning on stopping search as soon as a job id is recognised, ie. already scraped job ad but the re-posting 
         #by companies means that i cant do this, i need to search through all each time
         #SOLUTION: use date posted? -> eg regex <span class="date">2 dagar sedan</span>
-#visit each job listing, extract job descriptions
+        # compare with current date and date of last run (will have to be saved)
+
+#2. visit each job listing, extract job descriptions
+ads = {}
+start = time.time()
+for key in links:
+    time.sleep(8)
+    url = links[key]
+    with request.urlopen(url) as f:
+        html = f.read().decode("utf-8")
+        html = html.replace("\n", " ")
+    #isolate description
+    desc = re.findall("<div class=\"jobsearch-JobComponent-description icl-u-xs-mt--md\">(.*?)<div class", html)[0]
+    desc = BeautifulSoup(desc, 'lxml').text   
+    ads[key] = desc
+print(time.time()-start) #1 sec per request roughly (8second lapse between requests)
+
+#regex if want to save job title and company name respectively
+#<h3 class="icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title">Machine Learning Engineer</h3>
+#<div class="icl-u-lg-mr--sm icl-u-xs-mr--xs">Darwin Recruitment</div>
+
+with open(homedir+"Data/descriptions.json", "w") as f:
+    json.dump(ads, f)
